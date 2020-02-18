51c93c5924f92fbfd5f133cddff9284ccdade751
정백윤 <by@tripstore.kr>
2020-02-18 14:55:19 +0900

    Member 클래스 생성자 추가

--- src/main/java/org/rnilbong/sampletargetserver/Member.java
+++ src/main/java/org/rnilbong/sampletargetserver/Member.java
@@ -4,7 +4,14 @@ import lombok.ToString;
 
 @ToString
 public class Member {
+
     String id;
     String pw;
     int age;
+
+    Member(String id, String pw, int age) {
+        this.id = id;
+        this.pw = pw;
+        this.age = age;
+    }
 }
